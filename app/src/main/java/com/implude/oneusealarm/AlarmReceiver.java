e1bf59cb8662a6a3b7cc6d7b54045c0fd4a93d3d
Kim Haehwan <24457185+khaehwan@users.noreply.github.com>
2017-08-28 21:48:00 +0900

    알람1차구현중

---
+++ app/src/main/java/com/implude/oneusealarm/AlarmReceiver.java
@@ -0,0 +1,14 @@
+package com.implude.oneusealarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent mServiceintent = new Intent(context,AlarmSoundService.class);
+        context.startService(mServiceintent);
+    }
+}
