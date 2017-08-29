b36d64d520766f22ec1ed785ef71216ce83db83a
서강민 <tjrkdals613@gmail.com>
2017-08-29 11:55:21 +0900

    레이아웃 약간 수정, 메인액티비티 기본 코드 작성

--- app/src/main/java/com/implude/oneusealarm/MainActivity.java
+++ app/src/main/java/com/implude/oneusealarm/MainActivity.java
@@ -1,17 +1,60 @@
 package com.implude.oneusealarm;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
 
+    TimePicker timePicker;
+    Button buttonVib, buttonSound, buttonAddAlarm;
+
+    int hour=-1, minute=-1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timePicker = (TimePicker)findViewById(R.id.timePicker);
+        buttonVib = (Button)findViewById(R.id.VibButton);
+        buttonSound = (Button)findViewById(R.id.SoundButton);
+        buttonAddAlarm = (Button)findViewById(R.id.AddAlarmButton);
+
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int i, int i1) {
+                hour = timePicker.getHour();
+                minute = timePicker.getMinute();
+            }
+        });
+
+        buttonSound.setShadowLayer(30, 10, 10, Color.BLACK);
+
+        buttonVib.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        buttonSound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        buttonAddAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
