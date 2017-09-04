f5b8e64c53892df816c8a6f1025e967e05b9b051
서강민 <tjrkdals613@gmail.com>
2017-09-04 22:21:51 +0900

    애니메이션 파일 추가

--- app/src/main/java/com/implude/oneusealarm/MainActivity.java
+++ app/src/main/java/com/implude/oneusealarm/MainActivity.java
@@ -8,6 +8,7 @@ import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TimePicker;
