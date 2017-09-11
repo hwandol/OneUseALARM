3906a56d3a53cf473b8ea3448214abc118e73f6e
Kim Haehwan <24457185+khaehwan@users.noreply.github.com>
2017-09-11 20:26:09 +0900

    알람종료화면2

--- app/src/main/java/com/implude/oneusealarm/AlarmPlayActivity.java
+++ app/src/main/java/com/implude/oneusealarm/AlarmPlayActivity.java
@@ -1,5 +1,6 @@
 package com.implude.oneusealarm;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,18 +17,23 @@ public class AlarmPlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_play);
 
+
         TextView nowHour = (TextView) findViewById(R.id.nowHour);
         TextView nowMin = (TextView) findViewById(R.id.nowMin);
         Button stopBtn = (Button)findViewById(R.id.stopAlmBtn);
+        TextView nowDay = (TextView)findViewById(R.id.nowDate);
 
         long now = System.currentTimeMillis();
         Date date = new Date(now);
         SimpleDateFormat hour = new SimpleDateFormat("hh");
         SimpleDateFormat min = new SimpleDateFormat("mm");
+        SimpleDateFormat day = new SimpleDateFormat("mm/dd");
         String sHour =hour.format(date);
         String sMin =min.format(date);
+        String sDay = day.format(date);
         nowHour.setText(sHour);
         nowMin.setText(sMin);
+        nowDay.setText(sDay);
 
         stopBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,4 +43,6 @@ public class AlarmPlayActivity extends AppCompatActivity {
         });
 
     }
+
+
 }
