e939859f5fa9ad7a6d0a62d9d072a2a1abe45a84
Kim Haehwan <24457185+khaehwan@users.noreply.github.com>
2017-09-14 16:22:48 +0900

    알람 소리 진동기능 추가

--- app/src/main/java/com/implude/oneusealarm/AlarmPlayActivity.java
+++ app/src/main/java/com/implude/oneusealarm/AlarmPlayActivity.java
@@ -1,6 +1,8 @@
 package com.implude.oneusealarm;
 
+import android.content.Context;
 import android.media.MediaPlayer;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,7 +14,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class AlarmPlayActivity extends AppCompatActivity {
-
+    public static final int MODE_SOUND=1, MODE_VIB=2;
 
     MediaPlayer mediaPlayer;
 
@@ -20,9 +22,25 @@ public class AlarmPlayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_play);
-
+        final Vibrator vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
 
         int mode = getIntent().getExtras().getInt("Mode");
+
+        if(mode == MODE_SOUND){
+            Toast.makeText(getApplicationContext(),"소리알람이 울립니다",Toast.LENGTH_LONG).show();
+            mediaPlayer = MediaPlayer.create(this, R.raw.apple_ring);
+            mediaPlayer.setLooping(true);
+            mediaPlayer.start();
+
+        }else if(mode == MODE_VIB){
+            Toast.makeText(getApplicationContext(),"진동알람이 울립니다.",Toast.LENGTH_LONG).show();
+            vibrator.vibrate(new long[]{100,1000,100,500,100,500,100,1000},0);
+        }else{
+            Toast.makeText(getApplicationContext(),"오류로 인해 소리/진동이 설정되지 않았습니다.",Toast.LENGTH_LONG).show();
+        }
+
+
+
         Button stopBtn = (Button)findViewById(R.id.stopAlmBtn);
 
         TextView nowTime = (TextView) findViewById(R.id.nowTime);
@@ -42,6 +60,8 @@ public class AlarmPlayActivity extends AppCompatActivity {
         stopBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mediaPlayer.stop();
+                vibrator.cancel();
                 finish();
             }
         });
